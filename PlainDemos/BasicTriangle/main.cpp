2ee71f03efb518b03f79a3f196b960469ad89472
wpsperson <wpsperson@163.com>
2022-12-18 15:43:01 +0800

    validation layer

--- PlainDemos/BasicTriangle/main.cpp
+++ PlainDemos/BasicTriangle/main.cpp
@@ -9,6 +9,15 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
+const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+
+
 class HelloTriangleApplication {
 public:
     void run()
@@ -39,6 +48,11 @@ private:
 
     void createInstance()
     {
+        if (enableValidationLayers && !checkValidationLayerSupport())
+        {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Hello Triangle";
@@ -55,12 +69,45 @@ private:
         createInfo.pApplicationInfo = &appInfo;
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
-        createInfo.enabledLayerCount = 0; //TODO
+        if (enableValidationLayers)
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
         {
             throw std::runtime_error("failed to create instance");
         }
         checkSupportedExtensions();
+    }
+
+    bool checkValidationLayerSupport()
+    {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        std::vector<VkLayerProperties> availableLayerProperties(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayerProperties.data());
+        for (const char* layerName : validationLayers)
+        {
+            bool layerFound = false;
+            for (const VkLayerProperties& layerProperty : availableLayerProperties)
+            {
+                if (strcmp(layerProperty.layerName, layerName))
+                {
+                    layerFound = true;
+                    break;
+                }
+            }
+            if (!layerFound)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     void mainLoop()
